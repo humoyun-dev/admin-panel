a8d724b042464ed943843134144edd4cf3ac972e
Humoyunbek-Tursunniyazov <tursunniyazovhumoyunbek2007@gmail.com>
2023-02-09 20:36:56 +0500

    main

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 import { engine, create } from "express-handlebars";
+import * as dotenv from "dotenv";
+import flash from "connect-flash";
+import session from "express-session";
+
+// Routes
 import AppRoutes from "./routes/appRoute.js";
 import AuthRoutes from "./routes/auth.js";
+
+dotenv.config();
 
 const app = express();
 const hbs = create({
@@ -13,8 +21,32 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./views");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(AppRoutes);
 app.use(AuthRoutes);
+app.use(express.json());
+app.use(session({secret: "secret", resave: false, saveUninitialized: false}));
+// app.use(express.cookieParser("keyboard cat"));
+// app.use(express.session({ cookie: { maxAge: 60000 } }));
+app.use(flash());
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server is running in port: ${PORT}`));
+const startApp = () => {
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(
+      process.env.MONGO_URL,
+      {
+        useNewUrlParser: true,
+      },
+      () => console.log("Mongo DB connect")
+    );
+
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => console.log(`Server is running in port: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startApp();
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,64 @@
 import { Router } from "express";
+import User from "../models/User.js";
+import bcrytp from "bcrypt";
+
 const router = Router();
+
+// Login
 
 router.get("/login", (req, res) => {
   res.render("auth/login", {
-    title: "Login"
+    title: "Login",
+    isLoging: true,
+    loginError: req.flash('loginError'),
   });
 });
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    req.flash("loginError", "All fields are required");
+    res.redirect("/login");
+  }
+
+  const existUser = await User.findOne({ email });
+  if (!existUser) {
+    console.log("User not found", req.body.email);
+    return false;
+  }
+
+  const isPassEqual = await bcrytp.compare(password, existUser.password);
+  if (!isPassEqual) {
+    console.log("Password not match");
+    return false;
+  }
+
+  // console.log(req.body);
+  res.redirect("/");
+});
+
+// Register
 
 router.get("/register", (req, res) => {
   res.render("auth/register", {
-    title: "Register"
+    title: "Register",
+    isRegister: true,
+    registerError: "Error",
   });
 });
 
+router.post("/register", async (req, res) => {
+  const hashedPassword = await bcrytp.hash(req.body.password, 10);
+  const userData = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: hashedPassword,
+  };
+  // console.log(userData);
+  res.redirect("/");
+  const user = await User.create(userData);
+});
+
 export default router;
- 
